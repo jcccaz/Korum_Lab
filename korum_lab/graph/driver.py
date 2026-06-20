943f98920b837c799352546457eeefa8009744a6
jcccaz <carlos22079@gmail.com>
2026-06-20 15:19:35 +0000

    Add Neo4j get_session() helper scoped to configured database

--- korum_lab/graph/driver.py
+++ korum_lab/graph/driver.py
@@ -1,5 +1,5 @@
 from neo4j import GraphDatabase
-from korum_lab.config import NEO4J_URI, NEO4J_AUTH
+from korum_lab.config import NEO4J_URI, NEO4J_AUTH, NEO4J_DATABASE
 
 class GraphConnection:
     """Singleton-style Graph Connection Manager"""
@@ -11,6 +11,10 @@ class GraphConnection:
             cls._driver = GraphDatabase.driver(NEO4J_URI, auth=NEO4J_AUTH)
         return cls._driver
     
+    @classmethod
+    def get_session(cls):
+        return cls.get_driver().session(database=NEO4J_DATABASE)
+
     @classmethod
     def close(cls):
         if cls._driver is not None:
